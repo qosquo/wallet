d50d6d32d9d3a22073fe20f92ba80272be800be2
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2024-10-08 14:13:18 +0500

    Implement NavBackStackEntry.fromRoute() function to get current route as nullable String
    Use it to compare with needed Screen class qualified name.

--- app/src/main/java/com/qosquo/wallet/WalletApp.kt
+++ app/src/main/java/com/qosquo/wallet/WalletApp.kt
@@ -15,14 +15,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -30,40 +29,16 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.qosquo.wallet.Dependencies.accountsViewModel
 import com.qosquo.wallet.ui.screens.TopLevelRoute
 import com.qosquo.wallet.ui.screens.Routes
-import com.qosquo.wallet.ui.screens.Screen
 import com.qosquo.wallet.ui.screens.Screens
 import com.qosquo.wallet.ui.screens.accounts.AccountsForm
 import com.qosquo.wallet.ui.screens.accounts.AccountsList
 import com.qosquo.wallet.ui.screens.categories.CategoriesList
-import kotlin.reflect.jvm.jvmName
 
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun WalletAppBar(
-    currentScreen: Screen,
-    canNavigateBack: Boolean,
-    navigateUp: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TopAppBar(
-        title = { Text(stringResource(currentScreen.resourceId)) },
-        modifier = modifier,
-        navigationIcon = {
-//            if (canNavigateBack) {
-//                IconButton(onClick = navigateUp) {
-//                    Icon(
-//                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
-//                        contentDescription = "back button"
-//                    )
-//                }
-//            }
-        }
-    )
+fun NavBackStackEntry?.fromRoute(): String? {
+    return this?.destination?.route?.substringBefore("?")?.substringBefore("/")
 }
 
 @SuppressLint("RestrictedApi")
@@ -89,6 +64,7 @@ fun WalletApp(
 
     // Get current back stack entry
     val backStackEntry by navController.currentBackStackEntryAsState()
+    print(backStackEntry?.destination?.route)
     var selectedNavItemIndex by rememberSaveable {
         mutableIntStateOf(0)
     }
@@ -96,18 +72,26 @@ fun WalletApp(
         topBar = {
             TopAppBar(
                 title = {
-                    val title = when (backStackEntry?.destination?.route) {
-                        Screens.Accounts.List::class.qualifiedName -> Screens.Accounts.List.title
-                        Screens.Accounts.Form::class.qualifiedName -> Screens.Accounts.Form.title
-                        Screens.Categories.List::class.qualifiedName -> Screens.Categories.List.title
-                        else -> { null }
-                    }
-                    title?.let { Text(it) }
+                    val title: Int? = backStackEntry?.fromRoute().let {
+                            when (it) {
+                                Screens.Accounts.List::class.qualifiedName ->
+                                    R.string.accounts_list
+                                Screens.Accounts.Form::class.qualifiedName ->
+                                    R.string.accounts_form
+                                Screens.Categories.List::class.qualifiedName ->
+                                    R.string.categories_list
+                                else -> { null }
+                            }
+                        }
+                    title?.let { Text(stringResource(id = it)) }
                 },
                 navigationIcon = {
-                    val canNavigateBack: Boolean = when(backStackEntry?.destination?.route) {
-                        Screens.Accounts.Form::class.qualifiedName -> true
-                        else -> { false }
+                    val canNavigateBack: Boolean = backStackEntry?.fromRoute().let {
+                        when (it) {
+                            Screens.Accounts.Form::class.qualifiedName ->
+                                true
+                            else -> { false }
+                        }
                     }
                     if (canNavigateBack) {
                         IconButton(onClick = {
@@ -175,7 +159,9 @@ fun WalletApp(
                     AccountsList(
                         onEvent = accountsViewModel::onEvent,
                         onActionButtonClicked = { isEditing ->
-                                navController.navigate(Screens.Accounts.Form)
+                            navController.navigate(Screens.Accounts.Form(
+                                null
+                            ))
 //                            if (isEditing) {
 //                                navController.navigate(Screen.Accounts.Edit.route)
 //                            } else {
--- app/src/main/java/com/qosquo/wallet/ui/screens/Screen.kt
+++ app/src/main/java/com/qosquo/wallet/ui/screens/Screen.kt
@@ -3,57 +3,29 @@ package com.qosquo.wallet.ui.screens
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.qosquo.wallet.R
+import com.qosquo.wallet.model.Account
 import kotlinx.serialization.Serializable
 
-sealed class Screens {
+sealed interface Screens {
     @Serializable
-    sealed class Accounts(val title: String) {
+    sealed class Accounts {
         @Serializable
-        data object List : Accounts("Accounts")
+        object List
         @Serializable
-        data object Form : Accounts("Edit account")
+        data class Form(val accountId: Long?)
     }
     @Serializable
-    sealed class Categories(val title: String) {
+    sealed class Categories {
         @Serializable
-        data object List : Categories("Categories")
+        object List
     }
 }
 
-sealed class Routes {
+sealed interface Routes {
     @Serializable
     object Accounts
     @Serializable
     object Categories
-}
-
-sealed class Screen (
-    open val route: String,
-    @StringRes open val resourceId: Int
-) {
-
-    // Screen routes
-    sealed class Accounts() : Screen(
-//        R.drawable.bank_fill,
-        "accounts",
-        R.string.accounts
-    ) {
-        data object List : Screen("accounts/list", R.string.accounts_list)
-        data object Create : Screen("accounts/create", R.string.accounts_create)
-        data object Edit : Screen("accounts/edit", R.string.accounts_edit)
-    }
-
-    sealed class Categories() : Screen(
-//        R.drawable.chart_bar_fill,
-        "categories",
-        R.string.categories
-    ) {
-        data object List : Screen("categories/list", R.string.categories_list)
-    }
-
-    // Graph routes
-    data object AccountsNav : Screen("ACCOUNTS_NAV_GRAPH", R.string.accounts)
-    data object CategoriesNav : Screen("CATEGORIES_NAV_GRAPH", R.string.categories)
 }
 
 data class TopLevelRoute<T : Any>(
