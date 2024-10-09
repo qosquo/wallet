be936220b401939275aaae3f33ce69769a45db8b
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2024-10-09 17:59:28 +0500

    Add TabRows in CategoriesList

--- app/src/main/java/com/qosquo/wallet/ui/screens/categories/CategoriesList.kt
+++ app/src/main/java/com/qosquo/wallet/ui/screens/categories/CategoriesList.kt
@@ -1,26 +1,38 @@
 package com.qosquo.wallet.ui.screens.categories
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.Tab
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.qosquo.wallet.Dependencies
 import com.qosquo.wallet.Event
 import com.qosquo.wallet.ui.screens.Screens
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -29,6 +41,15 @@ fun CategoriesList(
     onNavigate: (id: Long?) -> Unit
 ) {
     val state by Dependencies.categoriesViewModel.state.collectAsStateWithLifecycle()
+
+    val tabRows = listOf(
+        "Expenses",
+        "Income"
+    )
+    val pagerState = rememberPagerState {
+        tabRows.size
+    }
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(onClick = {
@@ -38,13 +59,52 @@ fun CategoriesList(
             }
         }
     ) {
-//        LazyColumn(
-//            contentPadding = PaddingValues(16.dp),
-//            modifier = Modifier.fillMaxSize()//.padding(padding)
-//        )  {
-//            items(state.categories) {
-//
-//            }
-//        }
+        var selectedTabIndex by remember {
+            mutableIntStateOf(0)
+        }
+
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val scope = rememberCoroutineScope()
+
+            LaunchedEffect(pagerState.currentPage) {
+                selectedTabIndex = pagerState.currentPage
+            }
+            TabRow(selectedTabIndex = selectedTabIndex) {
+                tabRows.forEachIndexed {index, text ->
+                    Tab(
+                        selected = index == selectedTabIndex,
+                        text = {
+                            Text(text = text)
+                        },
+                        onClick = {
+                            scope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                            selectedTabIndex = index
+                        }
+                    )
+                }
+            }
+
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                LazyColumn(
+                    contentPadding = PaddingValues(16.dp),
+                    modifier = Modifier.fillMaxSize()
+                )  {
+                    items(state.categories) {
+
+                    }
+                }
+            }
+
+        }
+
     }
 }
